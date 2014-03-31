64ccdca92dc2cc2f5eca5c3e44d4fa7eaaafdfea
Alexander Chernikov <alerkesi@gmail.com>
2014-03-31 17:29:23 +0400

    Города попап.

--- js/basket.js
+++ js/basket.js
@@ -1,10 +1,16 @@
 $(document).ready(function ($) {
-    $popup = $('.popup-out');
+
+    var popupOut = '<div class="popup-out" style="display: none;"></div>';
+    $('body').append(popupOut);
+
+    $popup = $('.popup');
     $popup.on('click', '.popup-close', function () {
         $popup.hide();
+        $('.popup-out').hide();
         });
     $('.js-region').click(function (e) {
         e.stopPropagation();
         $('#cities').show();
+        $('.popup-out').show();
     });
 });
