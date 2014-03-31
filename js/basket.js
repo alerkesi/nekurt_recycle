e7432e01571a8e82ddc088f12d6544ba8e1e5962
Alexander Chernikov <alerkesi@gmail.com>
2014-03-31 19:10:24 +0400

    Поиск по городам

--- js/basket.js
+++ js/basket.js
@@ -10,10 +10,28 @@ $(document).ready(function ($) {
     $popup.on('click', '.popup-close', function () {
         $popup.hide();
         $popupOut.hide();
-        });
+    });
     $('.js-region').click(function (e) {
         e.stopPropagation();
         $('#cities').show();
         $popupOut.show();
     });
+    $('#city-search').keyup(function () {
+        var regex = $(this).val().toLowerCase();
+        var $cities = $('.city-name');
+
+        if (regex === '') {
+            $cities.show();
+            return;
+        }
+        //$cities.hide();
+        $cities.each(function(){
+            if ($(this).text().toLowerCase().indexOf(regex) > -1) {
+                $(this).show();
+            } else {
+                $(this).hide();
+            }
+        })
+
+    })
 });
